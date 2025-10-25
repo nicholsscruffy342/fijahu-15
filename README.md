b2a135d37741624de735041f49d632c348150128
zemguimaraes <josemaria_vasconcelosguimaraes@hotmail.com>
2025-10-25 16:34:42 +0000

    Initial commit: javascript project

---
+++ README.md
@@ -0,0 +1,17 @@
+# fijahu-15
+
+A simple Javascript project.
+
+## Description
+This is an auto-generated project demonstrating basic javascript functionality.
+
+## Running
+```bash
+# See the main file: index.js
+```
+
+## Language
+- Javascript
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * fijahu-15 - JavaScript Implementation
+ * A simple Hello World program
+ */
+
+function main() {
+    console.log("Hello, World!");
+    console.log("Welcome to fijahu-15");
+    
+    // Simple calculation
+    const numbers = [1, 2, 3, 4, 5];
+    const total = numbers.reduce((sum, num) => sum + num, 0);
+    console.log(`Sum of numbers: ${total}`);
+}
+
+main();
